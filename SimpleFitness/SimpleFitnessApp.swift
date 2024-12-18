5c2442d7bc41bfd0c2aa0cb894ad4271a7fbdb19
rodrigo <coopercine@gmail.com>
2024-12-18 14:32:18 +1100

    Initial Commit

---
+++ SimpleFitness/SimpleFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFitnessApp.swift
+//  SimpleFitness
+//
+//  Created by Rodrigo on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleFitnessTests/SimpleFitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFitnessTests.swift
+//  SimpleFitnessTests
+//
+//  Created by Rodrigo on 18/12/2024.
+//
+
+import Testing
+@testable import SimpleFitness
+
+struct SimpleFitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
